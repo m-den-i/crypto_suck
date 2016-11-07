df0d0fe2dff3ff9ee284a1020838aabc3a4a00d2
alexey <suharevichalexey@gmail.com>
2016-11-07 18:30:43 +0300

    Add StackWidget and dummy DocumentsWidgets

--- ui.py
+++ ui.py
@@ -4,10 +4,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class ClientApp(QtWidgets.QWidget):
+    STACK, LOGIN, VERIFY, DOCUMENTS = range(4)
+
     def __init__(self, client, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.client = client
         self.stacked = QtWidgets.QStackedLayout(self)
+
+        self.stack = StackWidget(self)
+        self.stacked.addWidget(self.stack)
 
         self.login = LoginWidget(self)
         self.stacked.addWidget(self.login)
@@ -15,19 +20,50 @@ class ClientApp(QtWidgets.QWidget):
         self.verify = CodeVerificationWidget(self)
         self.stacked.addWidget(self.verify)
 
+        self.documents = DocumentsWidget(self)
+        self.stacked.addWidget(self.documents)
+
+    def set_stack(self, encryption, verification):
+        self.client.use_encryption = encryption
+        self.client.use_verification = verification
+        self.client.connect()
+        self.stacked.setCurrentIndex(self.LOGIN)
+
     def on_login(self, login, password):
         if self.client.login(login, password):
-            self.stacked.setCurrentIndex(1)
+            self.stacked.setCurrentIndex(self.VERIFY if self.client.use_verification else self.DOCUMENTS)
         else:
             # TODO: show an error message
-            self.stacked.setCurrentIndex(0)
+            self.stacked.setCurrentIndex(self.LOGIN)
 
     def on_verify(self, code):
         if self.client.verify(code):
-            self.stacked.setCurrentIndex(2)
+            self.stacked.setCurrentIndex(self.DOCUMENTS)
         else:
             # TODO: show an error message
-            self.stacked.setCurrentIndex(1)
+            self.stacked.setCurrentIndex(self.VERIFY)
+
+
+class StackWidget(QtWidgets.QWidget):
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+        layout = QtWidgets.QVBoxLayout(self)
+
+        self.encryption_check = QtWidgets.QCheckBox('Encryption', checked=True)
+        layout.addWidget(self.encryption_check)
+
+        self.verify_code = QtWidgets.QCheckBox('Verify')
+        layout.addWidget(self.verify_code)
+
+        self.ok = QtWidgets.QPushButton('Ok')
+        self.ok.clicked.connect(self.submit)
+        layout.addWidget(self.ok)
+
+    def submit(self):
+        self.app.set_stack(self.encryption_check.isChecked(),
+                           self.verify_code.isChecked())
 
 
 class LoginWidget(QtWidgets.QWidget):
@@ -72,3 +108,13 @@ class CodeVerificationWidget(QtWidgets.QWidget):
 
     def submit(self):
         self.app.verify(self.code_edit.text())
+
+
+class DocumentsWidget(QtWidgets.QWidget):
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+        layout = QtWidgets.QVBoxLayout(self)
+
+        layout.addWidget(QtWidgets.QLabel('Well done!'))
