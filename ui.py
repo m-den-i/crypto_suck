6e51f525bb18ca9a038344733e3afb73f7f83022
alexey <suharevichalexey@gmail.com>
2016-11-26 16:59:04 +0300

    Refactor gui: use pyqt signals and slots

--- ui.py
+++ ui.py
@@ -10,7 +10,7 @@ class ClientApp(QtWidgets.QWidget):
     def __init__(self, client, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.client = client
-        self.cycle = True
+        self.updating = False
 
         layout = QtWidgets.QVBoxLayout(self)
         self.error_message = QtWidgets.QLabel()
@@ -19,19 +19,24 @@ class ClientApp(QtWidgets.QWidget):
         self.stacked = QtWidgets.QStackedLayout()
         layout.addLayout(self.stacked)
 
-        self.stack = StackWidget(self)
+        self.stack = StackWidget()
+        self.stack.submited.connect(self.set_stack)
         self.stacked.addWidget(self.stack)
 
-        self.login = LoginWidget(self)
+        self.login = LoginWidget()
+        self.login.submitted.connect(self.on_login)
         self.stacked.addWidget(self.login)
 
-        self.verify = CodeVerificationWidget(self)
+        self.verify = CodeVerificationWidget()
         self.stacked.addWidget(self.verify)
 
-        self.documents = DocumentsWidget(self)
+        self.documents = DocumentsWidget()
+        self.documents.add_file.connect(self.send_file)
+        self.documents.file_selected.connect(self.show_file)
         self.stacked.addWidget(self.documents)
 
-        self.file = FileWidget(self)
+        self.file = FileWidget()
+        self.file.go_back.connect(self.show_documents)
         self.stacked.addWidget(self.file)
 
     def show_error(self, message):
@@ -44,12 +49,14 @@ class ClientApp(QtWidgets.QWidget):
         self.hide_error()
         self.stacked.setCurrentIndex(index)
 
+    @QtCore.pyqtSlot(bool, bool)
     def set_stack(self, encryption, verification):
         self.client.use_encryption = encryption
         self.client.use_verification = verification
         self.client.connect()
         self.change_view(self.LOGIN)
 
+    @QtCore.pyqtSlot(str, str)
     def on_login(self, login, password):
         if not login or not password:
             self.show_error('Login and/or password cannot be empty.')
@@ -63,6 +70,7 @@ class ClientApp(QtWidgets.QWidget):
         except ServerError as e:
             self.show_error(str(e))
 
+    @QtCore.pyqtSlot(str)
     def on_verify(self, code):
         try:
             self.client.verify(code)
@@ -72,25 +80,35 @@ class ClientApp(QtWidgets.QWidget):
 
     def show_documents(self):
         self.change_view(self.DOCUMENTS)
-        self.documents.show_documents(self.client.get_files())
-        if self.cycle:
-            self.cycle = False
+        self.documents.display(self.client.get_files())
+
+        if not self.updating:
+            self.updating = True
             self.update_documents()
+
+    @QtCore.pyqtSlot(str)
+    def send_file(self, filename):
+        self.client.send_file(filename)
 
     @QtCore.pyqtSlot()
     def update_documents(self):
-        self.documents.show_documents(self.client.get_files())
+        self.documents.display(self.client.get_files())
         QtCore.QTimer.singleShot(1000, self.update_documents)
 
+    @QtCore.pyqtSlot(str)
     def show_file(self, name):
         self.change_view(self.FILE)
-        self.file.show_file(self.client.get_file(name))
+        self.file.display(self.client.get_file(name))
+
+    def sizeHint(self):
+        return QtCore.QSize(400, 300)
 
 
 class StackWidget(QtWidgets.QWidget):
-    def __init__(self, app):
+    submited = QtCore.pyqtSignal(bool, bool)
+
+    def __init__(self):
         super().__init__()
-        self.app = app
 
         layout = QtWidgets.QVBoxLayout(self)
 
@@ -100,103 +118,114 @@ class StackWidget(QtWidgets.QWidget):
         self.verify_code = QtWidgets.QCheckBox('Verify')
         layout.addWidget(self.verify_code)
 
-        self.ok = QtWidgets.QPushButton('Ok')
-        self.ok.clicked.connect(self.submit)
-        layout.addWidget(self.ok)
+        ok_button = QtWidgets.QPushButton('Ok')
+        ok_button.clicked.connect(self.on_submit)
+        layout.addWidget(ok_button)
 
-    def submit(self):
-        self.app.set_stack(self.encryption_check.isChecked(),
+    def on_submit(self):
+        self.submited.emit(self.encryption_check.isChecked(),
                            self.verify_code.isChecked())
 
 
 class LoginWidget(QtWidgets.QWidget):
-    def __init__(self, app):
+    submitted = QtCore.pyqtSignal(str, str)
+
+    def __init__(self):
         super().__init__()
-        self.app = app
 
         self.setObjectName('Login')
-        self.setMinimumWidth(250)
+        self.setMinimumWidth(300)
 
         layout = QtWidgets.QFormLayout()
         self.setLayout(layout)
 
         self.login_edit = QtWidgets.QLineEdit()
+        self.login_edit.setMinimumWidth(200)
         layout.addRow('Login:', self.login_edit)
 
         self.password_edit = QtWidgets.QLineEdit()
         self.password_edit.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password_edit.setMinimumWidth(200)
         layout.addRow('Password:', self.password_edit)
 
-        self.submit_button = QtWidgets.QPushButton('Login')
-        self.submit_button.clicked.connect(self.on_submit)
-        layout.addWidget(self.submit_button)
+        submit_button = QtWidgets.QPushButton('Login')
+        submit_button.clicked.connect(self.on_submit)
+        layout.addWidget(submit_button)
 
     def on_submit(self):
-        self.app.on_login(self.login_edit.text(), self.password_edit.text())
+        self.submitted.emit(self.login_edit.text(), self.password_edit.text())
 
 
 class CodeVerificationWidget(QtWidgets.QWidget):
-    def __init__(self, app):
+    submitted = QtCore.pyqtSignal(str)
+
+    def __init__(self):
         super().__init__()
-        self.app = app
 
         layout = QtWidgets.QFormLayout(self)
 
         self.code_edit = QtWidgets.QLineEdit()
         layout.addRow('Code', self.code_edit)
 
-        self.submit_button = QtWidgets.QPushButton('OK')
-        self.submit_button.clicked.connect(self.submit)
-        layout.addWidget(self.submit_button)
+        submit_button = QtWidgets.QPushButton('OK')
+        submit_button.clicked.connect(self.on_submit)
+        layout.addWidget(submit_button)
 
-    def submit(self):
-        self.app.on_verify(self.code_edit.text())
+    def on_submit(self):
+        self.submitted.emit(self.code_edit.text())
 
 
 class DocumentsWidget(QtWidgets.QWidget):
-    def __init__(self, app):
+    add_file = QtCore.pyqtSignal(str)
+    file_selected = QtCore.pyqtSignal(str)
+
+    def __init__(self):
         super().__init__()
-        self.app = app
 
         layout = QtWidgets.QVBoxLayout(self)
 
         open_file = QtWidgets.QPushButton('Add file')
-        open_file.clicked.connect(self.select_file)
+        open_file.clicked.connect(self.open_file_dialog)
         layout.addWidget(open_file)
 
         layout.addWidget(QtWidgets.QLabel('Well done!'))
         self.list_view = QtWidgets.QListWidget()
-        self.list_view.itemDoubleClicked.connect(lambda: self.app.show_file(self.list_view.currentItem().text()))
+        self.list_view.itemDoubleClicked.connect(self.select_document)
         layout.addWidget(self.list_view)
 
-    def select_file(self):
+    def open_file_dialog(self):
         filename, _ = QtWidgets.QFileDialog.getOpenFileName()
         if filename:
-            self.app.client.send_file(filename)
-            self.app.update_documents()
+            self.add_file.emit(filename)
 
-    def show_documents(self, documents):
+    def select_document(self):
+        self.file_selected.emit(self.list_view.currentItem().text())
+
+    def display(self, documents):
         self.list_view.clear()
         self.list_view.addItems([doc['name'] for doc in documents])
 
 
 class FileWidget(QtWidgets.QWidget):
-    def __init__(self, app):
+    go_back = QtCore.pyqtSignal()
+
+    def __init__(self):
         super().__init__()
-        self.app = app
 
         layout = QtWidgets.QVBoxLayout(self)
 
-        back_button = QtWidgets.QPushButton('Back')
-        back_button.clicked.connect(self.app.show_documents)
-        layout.addWidget(back_button)
-
         self.title = QtWidgets.QLabel()
+        self.title.setStyleSheet("font-size: 18px; font-weight: bold;")
         self.label = QtWidgets.QLabel()
         layout.addWidget(self.title)
         layout.addWidget(self.label)
 
-    def show_file(self, data):
+        layout.addStretch()
+        back_button = QtWidgets.QPushButton('Back')
+        back_button.clicked.connect(self.go_back)
+        layout.addWidget(back_button)
+
+    def display(self, data):
         self.title.setText(data['name'])
         self.label.setText(data['content'])
 
